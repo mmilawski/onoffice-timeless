97275d537abf76f4d97f888263ccf68bd1929c48
ogzA <akankan.oguz@gmail.com>
2024-11-26 14:56:12 +0100

    Implemented new icons for the mobile burger menu

--- header.php
+++ header.php
@@ -97,17 +97,9 @@ if (empty($header_modules_left) && empty($header_modules_right)) {
            'oo_theme',
        ); ?></span>
 
+                        <svg class="c-main-nav__button-icon --open" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
 
-                        <svg class="c-main-nav__button-icon --open" width="32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 12">
-                            <line x1="3" y1="1" x2="15" y2="1" vector-effect="non-scaling-stroke" stroke-width="1" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-                            <line x1="6" y1="6" x2="15" y2="6" vector-effect="non-scaling-stroke" stroke-width="1" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-                            <line x1="9" y1="11" x2="15" y2="11" vector-effect="non-scaling-stroke" stroke-width="1" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                        <svg class="c-main-nav__button-icon --close" width="32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
-                            <line x1="1" y1="13" x2="13" y2="1" vector-effect="non-scaling-stroke" stroke-width="1" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-                            <line x1="1" y1="1" x2="13" y2="13"  vector-effect="non-scaling-stroke" stroke-width="1" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-
+                        <svg class="c-main-nav__button-icon --close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
 
 						</button>
 
