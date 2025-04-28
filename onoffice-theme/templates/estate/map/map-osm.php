a9fd9cc062a67399cccb61b86fb9146fb5337cdd
Monika Radermaker <72728343+mradermaker@users.noreply.github.com>
2025-04-28 14:35:06 +0200

    Bug/p#132815 map zoom (#100)
    * corrected max zoom value in maps
    
    * updated shared
    
    * updated shared
    
    * updated shared

--- onoffice-theme/templates/estate/map/map-osm.php
+++ onoffice-theme/templates/estate/map/map-osm.php
@@ -95,7 +95,7 @@ return function (EstateList $pEstatesClone) {
     wp_enqueue_script('oo-init-open-street-map-marker-cluster');
     ?>
 
-    <div class="c-map --is-open-street-map" data-max-zoom="15" data-marker-color="<?php echo $primary_color; ?>" style="width: 100%;">
+    <div class="c-map --is-open-street-map" data-max-zoom="12" data-marker-color="<?php echo $primary_color; ?>" style="width: 100%;">
         <?php foreach ($property_data as $property) {
 
             $position = $property['position'] ?? [];
