9f42950110debb224a9cb4ec95d753320fb4d1d6
andernath <a.moeller@onoffice.de>
2025-09-10 11:39:33 +0200

    replace string escaping with string escaping + translation

--- blocks/reviews/google-review-card.php
+++ blocks/reviews/google-review-card.php
@@ -73,7 +73,7 @@ if (
                                 'oo_theme',
                             ); ?>"
                             aria-expanded="false" aria-controls="<?php echo $uniqid; ?>">
-                            <?php echo esc_html('Mehr anzeigen', 'oo_theme'); ?>
+                            <?php echo esc_html__('Mehr anzeigen', 'oo_theme'); ?>
                         </button>
                     </div>
                 <?php } ?>
