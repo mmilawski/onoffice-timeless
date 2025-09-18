8083be8447cdf99ec60535d4f47d66be8879ae31
afriedrichonoffice <a.friedrich@onoffice.com>
2025-09-18 12:05:47 +0200

    Format

--- onoffice-theme/templates/estate/property-details.php
+++ onoffice-theme/templates/estate/property-details.php
@@ -334,7 +334,9 @@ while ($current_property = $pEstates->estateIterator()) {
                                     'oo_theme',
                                 );
                                 $favorite_icon =
-                                    $favorite_label == 'Watchlist' ? 'bookmark' : 'heart';
+                                    $favorite_label == 'Watchlist'
+                                        ? 'bookmark'
+                                        : 'heart';
                             }
                             ?>
                             <button class="c-property-details__favorite c-icon-button" data-onoffice-property-id="<?php echo $property_id; ?>" aria-label="<?php echo $favorite_text; ?>">
