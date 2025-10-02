06ebe24b062fae163c44b1559613513feecd05c2
Aurelia Vogl <a.vogl@onoffice.de>
2025-10-02 13:20:00 +0200

    handle spacing

--- blocks/forms/forms-render.php
+++ blocks/forms/forms-render.php
@@ -15,12 +15,11 @@ $bg_color = $settings['bg_color'] ?? 'bg-transparent';
 ); ?> class="c-forms --is-<?php echo $type; ?>-form o-section --<?php echo $bg_color; ?>">
     <div class="c-forms__container o-container">
         <?php if (!empty($headline['text']) || !empty($text['wysiwyg'])) { ?>
-            <div class="c-forms__content o-row --position-center">
+            <div class="c-forms__content pt-5 o-col-md-5">
                 <?php if (!empty($headline['text'])) {
                     oo_get_template('components', '', 'component-headline', [
                         'headline' => $headline,
-                        'additional_headline_class' =>
-                            'c-forms__headline o-col-12 o-col-lg-10 o-col-xl-8',
+                        'additional_headline_class' => 'c-forms__headline',
                     ]);
                 } ?>
 
@@ -31,10 +30,9 @@ $bg_color = $settings['bg_color'] ?? 'bg-transparent';
                 <?php } ?>
             </div>
         <?php } ?>
-
         <?php if (!empty($shortcode)) { ?>
-            <div class="c-forms__wrapper o-row --position-center">
-                <div class="c-forms__form o-col-12 o-col-lg-10 o-col-xl-8">
+            <div class="c-forms__wrapper u-offset-md-1 o-col-md-6 --<?php echo $bg_color; ?>">
+                <div class="c-forms__form">
                     <?php echo do_shortcode($shortcode); ?>
                 </div>
             </div>
