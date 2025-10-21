41fc6799ffecd135a1c0ea397dc0faba222d712a
miauzjpg <postboote@gmail.com>
2025-10-21 10:00:40 +0200

    button colors

--- blocks/link-boxes/link-boxes-card.php
+++ blocks/link-boxes/link-boxes-card.php
@@ -9,157 +9,91 @@ $is_slider = filter_var($slider['slider'], FILTER_VALIDATE_BOOLEAN);
 <?php foreach ($boxes as $card) {
 
     // Content
-    $type = $card['type'] ?? 'image';
-    $image = $card['image'] ?? [];
-    $image_url = $image['url'] ?? null;
+    $type = $card['type'] ?? null;
+    $content = $card[$type];
+    $url = $content['url'];
     $text = $card['text'] ?? [];
     $headline = $card['headline'] ?? [];
     $link = $card['link'] ?? [];
-    $icon = $card['icon'] ?? [];
-    $icon_url = $icon['url'] ?? null;
-    $icon_alt = !empty($icon['alt'])
-        ? $icon['alt']
-        : (!empty($icon['title'])
-            ? $icon['title']
+    $alt_text = !empty($content['alt'])
+        ? $content['alt']
+        : (!empty($content['title'])
+            ? $content['title']
             : null);
+    $type = wp_check_filetype($url)['ext'] === 'svg' ? 'svg' : $type;
 
-    // Image width
-    $image_width_xs = '541';
-    $image_width_sm = '510';
-    $image_width_md = '333';
-    $image_width_lg = '450';
-    $image_width_xl = '350';
-    $image_width_xxl = '414';
-    $image_width_xxxl = '458';
-
-    // Icon width
-    $icon_width_xs = '477';
-    $icon_width_sm = '446';
-    $icon_width_md = '270';
-    $icon_width_lg = '386';
-    $icon_width_xl = '286';
-    $icon_width_xxl = '350';
-    $icon_width_xxxl = '394';
+    $dimensions =
+        $type === 'image'
+            ? [
+                '575' => ['w' => 541, 'h' => round((541 * 2) / 3)], // xs
+                '576' => ['w' => 510, 'h' => round((510 * 2) / 3)], // sm
+                '768' => ['w' => 333, 'h' => round((333 * 2) / 3)], // md
+                '992' => ['w' => 450, 'h' => round((450 * 2) / 3)], // lg
+                '1200' => ['w' => 350, 'h' => round((350 * 2) / 3)], // xl
+                '1400' => ['w' => 414, 'h' => round((414 * 2) / 3)], // xxl
+                '1600' => ['w' => 458, 'h' => round((458 * 2) / 3)], // xxxl
+            ]
+            : [
+                '575' => ['w' => 477, 'h' => round((477 * 2) / 3)], // xs
+                '576' => ['w' => 446, 'h' => round((446 * 2) / 3)], // sm
+                '768' => ['w' => 270, 'h' => round((270 * 2) / 3)], // md
+                '992' => ['w' => 386, 'h' => round((386 * 2) / 3)], // lg
+                '1200' => ['w' => 286, 'h' => round((286 * 2) / 3)], // xl
+                '1400' => ['w' => 350, 'h' => round((350 * 2) / 3)], // xxl
+                '1600' => ['w' => 394, 'h' => round((394 * 2) / 3)], // xxxl
+            ];
     ?>
     <article class="c-link-boxes-card <?php if ($is_slider) {
         echo '--on-slider c-slider__slide splide__slide';
     } ?>">
-        <?php if (!empty($image) || !empty($icon)) { ?>
-        <?php if (!empty($link['url'])) {
+        <?php if (!empty($content)) {
 
-            $button_text = $link['title']
-                ? $link['title']
-                : esc_html__('Mehr erfahren', 'oo_theme');
+            if (!empty($link['url'])) {
 
-            $aria_label = !empty($headline)
-                ? sprintf('%s zu %s', $button_text, $headline)
-                : $button_text;
-            ?>
+                $button_text = $link['title']
+                    ? $link['title']
+                    : esc_html__('Mehr erfahren', 'oo_theme');
+
+                $aria_label = !empty($headline)
+                    ? sprintf('%s zu %s', $button_text, $headline)
+                    : $button_text;
+                ?>
             <a class="c-link-boxes-card__link --has-<?php echo $type; ?>"
                 aria-label="<?php echo esc_attr($aria_label); ?>"
                 <?php echo oo_set_link_attr($link); ?>>
-        <?php
-        } else {
-             ?>
+            <?php
+            } else {
+                 ?>
             <div class="c-link-boxes-card__wrapper --has-<?php echo $type; ?>">
-        <?php
-        } ?>
-            <?php if (!empty($image) && $type === 'image'): ?>
-                <?php oo_get_template('components', '', 'component-image', [
-                    'image' => $image,
+            <?php
+            }
+            if ($type === 'svg') {
+                $svg_content = file_get_contents($url);
+                if (!empty($svg_content)) {
+                    $svg = new SimpleXMLElement($svg_content);
+                    $svg->addAttribute('role', 'img');
+                    $svg->addAttribute('aria-label', $alt_text);
+                    $svg->addAttribute('class', 'c-link-boxes-card__svg');
+                    echo "{$svg->asXML()}";
+                }
+            } elseif (!empty($content)) {
+                oo_get_template('components', '', 'component-image', [
+                    'image' => $content,
                     'picture_class' => 'c-link-boxes-card__picture o-picture',
                     'image_class' => 'c-link-boxes-card__image o-image',
-                    'dimensions' => [
-                        '575' => [
-                            'w' => $image_width_xs,
-                            'h' => round(($image_width_xs * 2) / 3),
-                        ],
-                        '1600' => [
-                            'w' => $image_width_xxxl,
-                            'h' => round(($image_width_xxxl * 2) / 3),
-                        ],
-                        '1400' => [
-                            'w' => $image_width_xxl,
-                            'h' => round(($image_width_xxl * 2) / 3),
-                        ],
-                        '1200' => [
-                            'w' => $image_width_xl,
-                            'h' => round(($image_width_xl * 2) / 3),
-                        ],
-                        '992' => [
-                            'w' => $image_width_lg,
-                            'h' => round(($image_width_lg * 2) / 3),
-                        ],
-                        '768' => [
-                            'w' => $image_width_md,
-                            'h' => round(($image_width_md * 2) / 3),
-                        ],
-                        '576' => [
-                            'w' => $image_width_sm,
-                            'h' => round(($image_width_sm * 2) / 3),
-                        ],
-                    ],
-                ]); ?>
-            <?php endif; ?>
-            
-            <?php if (!empty($icon) && $type === 'icon'):
-                $file_type = wp_check_filetype($icon_url);
-
-                if ($file_type['ext'] == 'svg') {
-                    $svg_content = file_get_contents($icon_url);
-                    if (!empty($svg_content)) {
-                        $svg = new SimpleXMLElement($svg_content);
-                        $svg->addAttribute('role', 'img');
-                        $svg->addAttribute('aria-label', $icon_alt);
-                        $svg->addAttribute('class', 'c-link-boxes-card__icon');
-                        echo "{$svg->asXML()}";
-                    }
-                } else {
-                    oo_get_template('components', '', 'component-image', [
-                        'image' => $icon,
-                        'picture_class' =>
-                            'c-link-boxes-card__picture o-picture',
-                        'image_class' => 'c-link-boxes-card__image o-image',
-                        'additional_cloudimg_params' => '&func=bound',
-                        'dimensions' => [
-                            '575' => [
-                                'w' => $icon_width_xs,
-                                'h' => round(($icon_width_xs * 2) / 3),
-                            ],
-                            '1600' => [
-                                'w' => $icon_width_xxxl,
-                                'h' => round(($icon_width_xxxl * 2) / 3),
-                            ],
-                            '1400' => [
-                                'w' => $icon_width_xxl,
-                                'h' => round(($icon_width_xxl * 2) / 3),
-                            ],
-                            '1200' => [
-                                'w' => $icon_width_xl,
-                                'h' => round(($icon_width_xl * 2) / 3),
-                            ],
-                            '992' => [
-                                'w' => $icon_width_lg,
-                                'h' => round(($icon_width_lg * 2) / 3),
-                            ],
-                            '768' => [
-                                'w' => $icon_width_md,
-                                'h' => round(($icon_width_md * 2) / 3),
-                            ],
-                            '576' => [
-                                'w' => $icon_width_sm,
-                                'h' => round(($icon_width_sm * 2) / 3),
-                            ],
-                        ],
-                    ]);
-                }
-            endif; ?>
+                    'additional_cloudimg_params' =>
+                        $type === 'image' ? '&func=bound' : '',
+                    'dimensions' => $dimensions,
+                ]);
+            }
+            ?>
         <?php if (!empty($link['url'])) { ?>
             </a>
         <?php } else { ?>
             </div>
         <?php } ?>
-        <?php } ?>
+        <?php
+        } ?>
 
         <?php if (
             !empty($headline) ||
