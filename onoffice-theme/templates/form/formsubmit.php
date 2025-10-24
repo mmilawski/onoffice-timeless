cf8c16d07822e9af94ef27ef11f4e5a5c40ba294
andernath <a.moeller@onoffice.de>
2025-10-24 11:37:28 +0200

    fix: wrap usercentrics recaptcha activation scripts in IIFE to enable all disabled buttons and avoid redeclaration errors

--- onoffice-theme/templates/form/formsubmit.php
+++ onoffice-theme/templates/form/formsubmit.php
@@ -59,10 +59,8 @@ if ($pForm->needsReCaptcha() && $key !== '') {
   ); ?>" data-size="invisible">
 	</div>
     <script>
-        // Prevents the script from being initialized more than once on the same page.
-        if (!window.onOfficeFormInitialized) {
-            window.onOfficeFormInitialized = true;
-
+        // Script needs to run on every from even on the same page.
+        (() => {
             // Define the Usercentrics template IDs for Google Recaptcha and the desired consent state.
             const serviceConsents = [
                 { id: 'Hko_qNsui-Q', consent: true }, // Google Recaptcha v2
@@ -169,8 +167,7 @@ if ($pForm->needsReCaptcha() && $key !== '') {
                     onOffice.captchaControl(form, submitButtonElement);
                 }
             });
-
-        }
+        })();
 	</script>
 	<button class="c-form__button c-button <?php if (
      !empty($settings['bg_color'])
